a1ae04b69274387e808c3b52a025cbf4b41e4704
ZohebUpade <44241478+ZohebUpade@users.noreply.github.com>
2018-10-17 14:49:09 -0400

    Update FongRestController.java

--- src/main/java/com/fong/pizzashopapi/FongRestController.java
+++ src/main/java/com/fong/pizzashopapi/FongRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FongRestController {
 	@RequestMapping
 	String mapToHttp() {
-		return  "Welcome!";
+		return  "Hello World!";
 	}
 
 }
